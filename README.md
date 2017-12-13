9240cdbd4232ab69efc7366d055a356ce4936787
Song.Jin <songjin@hotmail.com>
2017-12-13 11:22:38 +1100

    make project lambda ready by installing dep in current dir/vendor and append it to sys.path

--- README.md
+++ README.md
@@ -1,11 +1,52 @@
 # ops-buildkite-reporter
 
-This is a Stat reporting tool that talks to BuildKite REST API V2 and generate
-csv formatted report for management to have a picture of the BK usage within
+This is a Stat reporting tool that talks to BuildKite API V2/GraphQL and generates
+reports in csv format for management to have a picture of the BK usage within
 MYOB
 
+
 ## Get Started
-To set BK_TOKEN as Env Var, please run
-`export BK_TOKEN=<your personal BK API token with all read related permissions>`
 
+- set `export BK_TOKEN=<the BuildKite API token>`
 
+   > token: your personal BK API token with all read related permissions
+
+- set `export BK_DRYRUN=False`
+
+   > feel free to set `BK_DRYRUN=True` if you want to save time or computationally
+expensive API hits, or decide it is not necessary to fetch up-to-date data from
+the API.
+
+- simply run `./team_pipeline_build_stat.py` to get CSV reports in local
+directory
+
+## TODO
+
+- More unit tests
+- CI pipeline
+- AWS integrations:
+  * get the BK_TOKEN from AWS-SSM
+  * Be able to run this program in AWS-Lambda
+  * AWS-CloudWatch can trigger the above AWS-Lambda weekly
+  * output the CSV report to a AWS-S3 version-enabled bucket
+  * setup SES (Simple Email Service) to send the CSV report to the Management
+  once CSV report is ready
+- CD pipeline to deploy the above mentioned stack
+
+## For Developers
+
+- To make it more compatible with AWS-Lambda, in term of dependency management.
+Please run
+  - `cd <the project's root folder>`
+  - `pip install <your new dependency> -t <./vendor>`
+
+  > If you want to add dependencies to the project AND you use `OSX` as
+operating system. Please consider add a cfg file temporarily, as suggested
+[here](https://stackoverflow.com/questions/24257803/distutilsoptionerror-must-supply-either-home-or-prefix-exec-prefix-not-both).
+Otherwise you will have an strange error due to a known issue with PIP.
+`distutils.errors.DistutilsOptionError: must supply either home or prefix/exec-prefix -- not both`
+
+  - After installing new dependencies, don't forget to remove the workaround file
+  `rm $HOME/.pydistutils.cfg`
+  - Remove unnecessary `whl` files to make your package slim
+  `rm ./vendor/*.whl`
--- team_pipeline_build_stat.py
+++ team_pipeline_build_stat.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python3
+from settings import TOKEN, DRYRUN
 import requests
 import json
 import os,re
-from exceptions import NoTeamError, GeneralApiError, EnvVarError
+from exceptions import NoTeamError, GeneralApiError
+from csv_ops import ProcessCsvFile
 
 GQL_QUERY = {"query": '''{
                   organization(slug:"myob") {
@@ -44,55 +46,44 @@ GQL_QUERY = {"query": '''{
                 }'''
             }
 
-def team_pipelines(url):
+def _team_pipelines(url):
     """
-        Input: BK API token and an Endpoint
+        Input: BK API token from Global var and an Endpoint url
         Output: A list of json result of
     """
-    token = os.environ['BK_TOKEN']
+
     headers = {}
-    headers['Authorization'] = "Bearer {}".format(token)
+    headers['Authorization'] = "Bearer {}".format(TOKEN)
     headers['Content-Type'] = 'application/json'
 
-    # result = ""
     params = []
     payload = GQL_QUERY
     try:
-        import json
         resp = requests.post(url,headers=headers,data=json.dumps(payload))
         return resp
     except Exception as e:
-        print("SOMETHING WRONG: ", e)
+        print("SOMETHING WENT WRONG...: ", e)
 
 def process_gql_resp(gql_resp):
     """
-        teams = {
-                slug,
-                pipeline_count
-            }
-
-        pipelines = {
-                team_slug,
-                    pipeline_slug,
-                        pass_builds, failed_builds, last_build_time(pipeline)
-            }
+        This module processes gql response JSON
+        Input:  graphQL response JSON
+        Output: List of Dictionaries including data for the CSV columns
     """
     teams = gql_resp['data']['organization']['teams']['edges']
     team_count = gql_resp['data']['organization']['teams']['count']
     if team_count < 1:
         raise NoTeamError("Critical: there should be at least 1 team")
-    # print(type(teams))
     result = []
     for team in teams:
         team_slug = team['node']['slug']
         team_pipelines = team['node']['pipelines']
-        # print(team_slug, "|||pipe count=", team_pipelines['count'])
         for team_pipe in team_pipelines['edges']:
             team_pipe_details = team_pipe['node']['pipeline']
             if len(team_pipe_details['builds']['edges']) > 0:
                 last_build_time = team_pipe_details['builds']['edges'][0]['node']['last_build_time']
             else:
-                last_build_time = "na"
+                last_build_time = "n/a"
             # print(
             #         team_slug,
             #         team_pipe_details['slug'],
@@ -101,51 +92,35 @@ def process_gql_resp(gql_resp):
             #         last_build_time
             #      )
 
-            result.append({"team_slug": team_slug,
-                    "pipe_slug": team_pipe_details['slug'],
-                    "pass": team_pipe_details['pass_builds']['pass_builds_count'],
-                    "fail": team_pipe_details['fail_builds']['fail_builds_count'],
-                    "last": last_build_time
-            })
-    print("pipeline count within our Org: ", len(result))
-    # print(result)
-        # team_slug = team['node']['slug']
-        # print("pipeline count=",team['node']['pipelines']['count'])
+            result.append(
+                    {
+                        "team_slug": team_slug,
+                        "pipe_slug": team_pipe_details['slug'],
+                        "pass": team_pipe_details['pass_builds']['pass_builds_count'],
+                        "fail": team_pipe_details['fail_builds']['fail_builds_count'],
+                        "last": last_build_time
+                    }
+                )
+    # print("pipeline count within our Org: ", len(result))
     return result
 
 
 if __name__ == '__main__':
-    if "BK_DRYRUN" in os.environ:
-        env_var_dr_value = os.environ['BK_DRYRUN']
-        if env_var_dr_value == "True":
-            DRYRUN = True
-        elif env_var_dr_value == "False":
-            DRYRUN = False
-        else:
-            raise EnvVarError(
-                    "expecting string True or False, but got something else"
-                )
-        print('env var acquired: BK_DRYRUN, value:', DRYRUN)
-    else:
-        DRYRUN = True
-
     url = "https://graphql.buildkite.com/v1"
     file_path = os.path.join(os.path.dirname(__file__), 'result.json')
     file_exists = os.path.isfile(file_path)
 
-    # DRYRUN = True
-    # DRYRUN = False
-    # file_exists = True
-    # file_exists = False
-
+    print("DRYRUN:", DRYRUN, type(DRYRUN), "||| file_exists:", file_exists)
+    # import sys
+    # sys.exit(0)
+####################
     if file_exists and DRYRUN:
         print(file_exists, DRYRUN, file_exists and DRYRUN)
         print("load json and process data, without running expensive api hit")
         gql_resp = json.load(open('result.json'))
     else:
         print("running expensive api hit")
-        r = team_pipelines(url)
-        # print(r.status_code)
+        r = _team_pipelines(url)
         if r.status_code == 200:
             try:
                 json_resp = r.json()
@@ -154,14 +129,16 @@ if __name__ == '__main__':
                 raise ValueError
             gql_resp = json_resp
         else:
-            raise GeneralApiError("not getting valid reply")
+            raise GeneralApiError(
+                "not getting valid reply" +
+                "status_code is {}".format(r.status_code))
     if not file_exists and DRYRUN:
         print("writing json resp to intermediate JSON file")
         with open('result.json', 'w') as out_file:
             json.dump(json_resp, out_file)
-
+##################
     processed_data = process_gql_resp(gql_resp)
-    from csv_ops import ProcessCsvFile
+
     p = ProcessCsvFile('.')
     p.prepare_result_file()
     p.write_csv_header([
